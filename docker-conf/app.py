bc55e5e7f6b564440031f78fba84010dbe46ebec
rehanwar <rehanwar@cisco.com>
2019-02-09 12:03:45 +0000

    Create Docker container

---
+++ docker-conf/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+
+
+app = Flask(__name__)
+api =   Api(app)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+    
